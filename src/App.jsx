b3d000c68c8950213b08ba05489464b705abd692
Jen Jones Arnesen <jennifer@dhis2.org>
2025-12-10 13:30:36 +0100

    chore: remove non-useful content from the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useDataQuery } from "@dhis2/app-runtime";
-import i18n from "@dhis2/d2-i18n";
 import React from "react";
 import classes from "./App.module.css";
-import PluginWrapper from "./PluginWrapper.jsx";
 import "./locales";
 
-const query = {
-  me: {
-    resource: "me",
-  },
-};
-
 const MyApp = () => {
-  const { error, loading, data } = useDataQuery(query);
-
-  if (error) {
-    return <span>{i18n.t("ERROR")}</span>;
-  }
-
-  if (loading) {
-    return <span>{i18n.t("Loading...")}</span>;
-  }
-
   return (
     <div className={classes.container}>
-      <h1>{i18n.t("Hello {{name}}", { name: data.me.name })}</h1>
-      <PluginWrapper
-        dashboardItemId={"dashboarditem-1234"}
-        dashboardItemFilters={[]}
-        dashboardMode={"edit"}
-        setDashboardItemDetails={Function.prototype}
-      />
+      <h1>Prediction Visualizer</h1>
+      <p>This is the Prediction Visualizer app.</p>
     </div>
   );
 };
